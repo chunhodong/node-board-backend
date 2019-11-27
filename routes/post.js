16db6911320ec0ceae35b4927d579d6fdacc3ea4
chunhodong <yhms4432@naver.com>
2019-11-28 00:46:06 +0900

    s3이미지업로드

--- routes/post.js
+++ routes/post.js
@@ -8,8 +8,8 @@ const AWS = require('aws-sdk');
 const multerS3 = require('multer-s3');
 
 AWS.config.update({
-    accessKeyId:'AKIAJGLX6E2LOUVSXNBA',
-    secretAccessKey:'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',
+    accessKeyId:process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey:process.env.S3_SECRET_ACCESS_KEY,
     region:'ap-northeast-2',
 });
 
