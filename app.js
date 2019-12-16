588d07fcfd7c2d22b3f4787fd608be723fe15038
ParkDonggi <yhms4432@ssu.ac.kr>
2019-12-16 17:08:30 +0900

    FEE

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-console.log('hi nice!!');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
